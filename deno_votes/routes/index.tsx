7fc645e56d035447873c35bbbd8805e7c0620d8b
JeongHeon Woo <email@wookingwoo.com>
2023-06-26 03:39:32 +0900

    Start deno project

---
+++ deno_votes/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Head } from "https://deno.land/x/fresh@1.2.0/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>
+          Deno Vote - wookingwoo
+        </title>
+      </Head>
+      <h1>
+        test page for testing deno
+      </h1>
+    </>
+  );
+}
